9d4a23193974f01e3a521929bb65b2d6cdca841d
Jamie Neubert Pedersen <jamieneubert@gmail.com>
2013-04-10 13:24:01 +0200

    add MMAS to new project
    Transfered from the old one

---
+++ src/nature/MinMaxAntSystemBitString.java
@@ -0,0 +1,59 @@
+package nature;
+
+
+
+public class MinMaxAntSystemBitString extends Algorithm<BitString> {
+	private FitnessGoal<BitString> fitnessGoal;
+	private BitString current;
+	private double rho;
+	private double[] pheromone;
+
+	public MinMaxAntSystemBitString(ProgressListener<BitString> progressListener, FitnessGoal<BitString> fitnessGoal, BitString bitString, double globalMutationProbability, double rho) {
+		super(progressListener);
+		
+		this.rho = rho;
+		this.fitnessGoal = fitnessGoal;
+		this.current = bitString;
+	}
+	
+	private void updatePheromones(BitString bitString) {
+		boolean[] string = bitString.getString();
+		double tauMin = 1.0 / bitString.length();
+		double tauMax = 1.0 - 1.0 / bitString.length();
+		for (int i = 0; i < pheromone.length; i++) {
+			if (string[i]) {
+				pheromone[i] = Math.min((1.0 - rho) * pheromone[i] + rho, tauMax);
+			} else {
+				pheromone[i] = Math.max((1.0 - rho) * pheromone[i], tauMin);
+			}
+		}
+	}
+	
+
+	@Override
+	public void init() {
+		pheromone = new double[current.length()];
+		current = current.constructMutation(pheromone);
+		updatePheromones(current);
+	}
+
+	@Override
+	public void step(long iteration) {
+		BitString mutation = current.constructMutation(pheromone);
+
+		
+		if (fitnessGoal.compare(current, mutation) <= 0) {
+			current = mutation;
+
+			progressListener.select(current);
+			if (fitnessGoal.isOptimal(current)) {
+				progressListener.done();
+				cancel();
+			}
+		}
+		
+		updatePheromones(current);
+
+		
+	}
+}
