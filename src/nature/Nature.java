613ec16155bcf6c3fac336a416a008b2671edc63
Jamie Neubert Pedersen <jamieneubert@gmail.com>
2013-04-10 10:57:17 +0200

    edit visual part
    Tried to add an extra window

--- src/nature/Nature.java
+++ src/nature/Nature.java
@@ -5,20 +5,29 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 import nature.ui.NaturePanel;
+import nature.ui.OnionVisualizer;
 
 public class Nature implements Runnable {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Nature());
 	}
-
+	
+	
 	@Override
 	public void run() {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(new NaturePanel());
 		frame.pack();
 		frame.setMinimumSize(frame.getSize());
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		
+		JFrame visualizer = new JFrame();
+		visualizer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		visualizer.add(new OnionVisualizer());
+		visualizer.pack();
+		visualizer.setMinimumSize(visualizer.getSize());
+		visualizer.setLocationRelativeTo(frame);
+		visualizer.setVisible(true);
 	}
 }
