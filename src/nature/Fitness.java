abef8fdbd8eb478deef7755e1a65ae979cd8fb1a
Salvador Jacobi <salvador.jacobi@gmail.com>
2013-04-16 22:07:23 +0200

    remove unused Fitness class

--- src/nature/Fitness.java
+++
@@ -1,7 +0,0 @@
-package nature;
-
-public abstract class Fitness<T> {
-	public abstract double evaluate(T state);
-	public abstract boolean isBetter(T original, T mutation);
-	public abstract double optimum();
-}
